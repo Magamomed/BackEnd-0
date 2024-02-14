9fc9d15a5eae04712dfa6877cbe51549e9272d4f
Magamomed <saparov-sultan2017@bk.ru>
2024-02-15 03:31:21 +0600

    dell shop

--- hw/shop/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Product(models.Model):
-    name = models.CharField(max_length=255)
-    description = models.CharField(max_length=1000)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return self.name
